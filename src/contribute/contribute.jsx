6c5647eb320cfb8fae080c6ace1ae9f303feb08d
Anna <annegg03@byu.edu>
2025-10-08 17:00:44 -0600

    storyPage rendered

--- src/contribute/contribute.jsx
+++ src/contribute/contribute.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export function Contribute() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>contribute displayed here</div>
+    <main className="container-fluid text-center">
+      <form method="get" action="storyPage.html" className="text-center container-fluid">
+            <div className="input-group mb-3">
+            <input className="mx-auto" style={{width: '300px'}} type="text" placeholder="Title" />
+            </div>
+            <div className="input-group mb-3">
+            <input className="mx-auto" style={{width: '300px'}} type="text" placeholder="Story Suggestion" />
+            </div>
+            <button type="submit" className="btn btn-outline-dark">Post</button>
+        </form>
     </main>
   );
 }
--- src/storyPage/storyPage.jsx
+++ src/storyPage/storyPage.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function StoryPage() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>storyPage displayed here</div>
+    <main className="container-fluid text-center">
+      <div className="container-fluid mx-auto" style={{maxWidth: '800px', padding: '20px'}}>
+            <div id="story" className="mx-auto text-center" style={{padding: '20px'}}>
+                <h2>Story Title</h2>
+                <h4>By Author</h4>
+                <p>Story premise and original context ideas
+                    (saved in database)
+                </p>
+            </div>
+            <div>
+                <h4>Idea #1 Title</h4>
+                <p>Idea text (saved in database, updated by websocket)</p>
+            </div>
+            <div>
+                <h4>Idea #2 Title</h4>
+                <p>Idea text (saved in database, updated by websocket)</p>
+            </div>
+        </div>
+        <div className="container-fluid text-center my-4">
+            <NavLink to="/contribute" className="btn btn-outline-dark btn-md fw-bold px-5 py-3">
+                Contribute
+            </NavLink>
+        </div>
     </main>
   );
 }
