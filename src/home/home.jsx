991155f5c500db40571d0417f471e1c749c5eaf3
Anna <annegg03@byu.edu>
2025-10-08 17:41:37 -0600

    home rendering

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Home() {
+
   return (
     <main className="container-fluid text-center">
       <div className="text-center container-fluid">
             <div className="story-links mx-auto text-start" style={{maxWidth: '800px'}}>
                 <div className="mb-4">
                     <div id="icon">icon placeholder</div>
-                    <a href="storyPage.html" className="fs-3 text-decoration-none text-dark fw-semibold">Your Story Idea #1 (still a link)</a>
+                    <NavLink to="/storyPage" className="fs-3 text-decoration-none text-dark fw-semibold">
+                        Your Story Idea #1 (still a link)
+                    </NavLink>
                     <p>Most recent idea suggestion title displayed here</p>
                 </div>
                 <div>
-                    <a href="storyPage.html" className="fs-3 text-decoration-none text-dark fw-semibold">Your Story Idea #2</a>
+                    <NavLink to="/storyPage" className="fs-3 text-decoration-none text-dark fw-semibold">
+                        Your Story Idea #2
+                    </NavLink>
                     <p>This will display links to other users' story idea pages
                     stored in the database. Most recent suggestions will be updated
                     in realtime with Websocket.</p>
