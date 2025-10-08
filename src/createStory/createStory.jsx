eb7d962f1119a292cc1f7a1ab1adb3d26b44cbd6
Anna <annegg03@byu.edu>
2025-10-08 17:03:14 -0600

    createStory rendered

--- src/createStory/createStory.jsx
+++ src/createStory/createStory.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export function CreateStory() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>CreateStory displayed here</div>
+    <main className="container-fluid text-center">
+      <form method="get" action="storyPage.html" className="text-center container-fluid">
+            <div>
+            <a href="https://lordicon.com/">Select icon</a>
+            <p>^Placeholder to use API to select icon for story</p>
+            </div>
+            <div className="input-group mb-3">
+            <input className="mx-auto" style={{width: '300px'}} type="text" placeholder="Story Title" />
+            </div>
+            <div className="input-group mb-3">
+            <input className="mx-auto" style={{width: '300px'}} type="text" placeholder="Story Premise" />
+            </div>
+            <button type="submit" className="btn btn-outline-dark">Post</button>
+        </form>
     </main>
   );
 }
