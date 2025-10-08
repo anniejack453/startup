4113ed94315719d85a481fb0b870c1b4cb830c7d
Anna <annegg03@byu.edu>
2025-10-08 16:33:04 -0600

    router configured

--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,63 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Browse } from './browse/browse';
+import { Contribute } from './contribute/contribute';
+import { CreateStory } from './createStory/createStory';
+import { Home } from './home/home';
+import { StoryPage } from './storyPage/storyPage';
 
 export default function App() {
   return (
-    <div className="body">
-        <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-purple d-flex justify-content-between">
-                <h1>StoryTogether</h1>
-                <menu className="navbar-nav d-flex flex-row">
-                <li className="nav-item">
-                    <a className="nav-link" href="home.html">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="browse.html">Browse</a>
-                </li>
-                <li className="nav-item">
-                </li>
-                </menu>
-                <div className="d-flex align-items-center ms-auto" style={{paddingRight: '20px'}}>
-                    <span className="me-3 fw-semibold" id="username">Username</span>
-                    <a className="btn btn-outline-dark btn-md fw-bold px-5 py-3" href="createStory.html">Create Your Story</a>
+    <BrowserRouter>
+        <div className="body">
+            <header className="container-fluid">
+                <nav className="navbar fixed-top navbar-purple d-flex justify-content-between">
+                    <h1>StoryTogether</h1>
+                    <menu className="navbar-nav d-flex flex-row">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="home">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="browse">
+                            Browse
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                    </li>
+                    </menu>
+                    <div className="d-flex align-items-center ms-auto" style={{paddingRight: '20px'}}>
+                        <span className="me-3 fw-semibold" id="username">Username</span>
+                        <a className="btn btn-outline-dark btn-md fw-bold px-5 py-3" href="createStory.html">Create Your Story</a>
+                    </div>
+                </nav>
+            </header>
+
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/home' element={<Home />} />
+                <Route path='/browse' element={<Browse />} />
+                <Route path='/contribute' element={<Contribute />} />
+                <Route path='/createStory' element={<CreateStory />} />
+                <Route path='/storyPage' element={<StoryPage />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer>
+                <div className="container-fluid">
+                    <span className="text-reset">Anna Egbert</span>
+                    <a className="text-reset" href="https://github.com/anniejack453/startup">GitHub</a>
                 </div>
-            </nav>
-        </header>
-
-        <main>App components go here</main>
-
-        <footer>
-            <div className="container-fluid">
-                <span className="text-reset">Anna Egbert</span>
-                <a className="text-reset" href="https://github.com/anniejack453/startup">GitHub</a>
-            </div>
-        </footer>
-    </div>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
