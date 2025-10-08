559099b463a9720f84a3e9f1143ddeed52d558e6
Anna <annegg03@byu.edu>
2025-10-08 16:42:38 -0600

    browse.jsx updated

--- src/browse/browse.jsx
+++ src/browse/browse.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Browse() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>browse displayed here</div>
+    <main>
+        <div className="text-center container-fluid">
+            <div className="story-links mx-auto text-start" style={{ maxWidth: '800px' }}>
+                <div className="mb-4">
+                    <div id="icon">icon placeholder</div>
+                    <NavLink to="/storyPage" className="fs-3 text-decoration-none text-dark fw-semibold">
+                        Someone's Story Idea #1 (still a link)
+                    </NavLink>
+                    <p>Most recent idea suggestion title displayed here</p>
+                </div>
+                <div>
+                    <NavLink to="/storyPage" className="fs-3 text-decoration-none text-dark fw-semibold">
+                        Someone's Story Idea #2
+                    </NavLink>
+                    <p>This will display links to other users' story idea pages
+                    stored in the database. Most recent suggestions will be updated
+                    in realtime with Websocket.</p>
+                </div>
+            </div>
+        </div>
     </main>
   );
 }
