a1557f2759024402e8a5ae90f0f51ab2f33877b2
Anna <annegg03@byu.edu>
2025-10-08 13:52:36 -0600

    app component header and footer rendered

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body">App will display here</div>;
+  return (
+    <div className="body">
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-purple d-flex justify-content-between">
+                <h1>StoryTogether</h1>
+                <menu className="navbar-nav d-flex flex-row">
+                <li className="nav-item">
+                    <a className="nav-link" href="home.html">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="browse.html">Browse</a>
+                </li>
+                <li className="nav-item">
+                </li>
+                </menu>
+                <div className="d-flex align-items-center ms-auto" style={{paddingRight: '20px'}}>
+                    <span className="me-3 fw-semibold" id="username">Username</span>
+                    <a className="btn btn-outline-dark btn-md fw-bold px-5 py-3" href="createStory.html">Create Your Story</a>
+                </div>
+            </nav>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer>
+            <div className="container-fluid">
+                <span className="text-reset">Anna Egbert</span>
+                <a className="text-reset" href="https://github.com/anniejack453/startup">GitHub</a>
+            </div>
+        </footer>
+    </div>
+  );
 }
