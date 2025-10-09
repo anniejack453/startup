19b47ab5c928ddc758e6a5c77574500d8c96ba10
Anna <annegg03@byu.edu>
2025-10-08 18:12:49 -0600

    login page updated, buttons go to home page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export function Login() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/home');
+  };
+
   return (
     <main className="container-fluid p-0 text-center">
         <div id="picture" className="picture-box"><img className="img-fluid w-100" src="scenery-art-wallpaper.jpg" alt="random" /></div>
         <div>
             <h1>Let's tell a story, together</h1>
-            <form method="get" action="home.html">
+            <form onSubmit={handleSubmit}>
                 <div className="input-group mb-3">
                     <span className="input-group-text">Email:</span>
                     <input className="form-control" type="text" placeholder="your@email.com" />
