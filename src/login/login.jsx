1b00c70ea56b3445a9f6ab647bc3afcbeb1f540d
Anna <annegg03@byu.edu>
2025-10-21 14:41:23 -0600

    login authentication started

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-export function Login() {
-  const navigate = useNavigate();
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate('/home');
-  };
-
+export function Login({userName, authState, onAuthChange}) {
   return (
     <main className="container-fluid p-0 text-center">
+      <div id="picture" className="picture-box"><img className="img-fluid w-100" src="scenery-art-wallpaper.jpg" alt="random" /></div>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Let's tell a story, together</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+    </main>
+  );
+    {/* <main className="container-fluid p-0 text-center">
         <div id="picture" className="picture-box"><img className="img-fluid w-100" src="scenery-art-wallpaper.jpg" alt="random" /></div>
         <div>
             <h1>Let's tell a story, together</h1>
@@ -27,6 +41,5 @@ export function Login() {
                 <button type="submit" className="btn btn-secondary">Create</button>
             </form>
         </div>
-    </main>
-  );
+    </main> */}
 }
